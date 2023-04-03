651540b9499d0e491d43bfc5d36579aecfa2f415
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2023-04-03 21:37:52 +0300

    [Salih] create contents endpoint

---
+++ src/api/contents/routes/contents.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/contents',
+    //  handler: 'contents.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
---
+++ src/api/contents/services/contents.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * contents service
+ */
+
+module.exports = () => ({});
